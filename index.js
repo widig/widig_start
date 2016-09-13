9212766f04b07d489d60eda48b22f675555986bd
widig <fkodama@gmail.com>
2016-09-13 10:06:37 -0700

    index of paths

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require("express");
 var app = express();
 
+var fs = require("fs");
 
 
-app.get("/",function(req,res) {
-	res.send("hello");
-	res.end();
-});
+var index = JSON.parse( fs.readFileSync("index.json","utf8") );
+if("get" in index) {
+	for(var key in index.get) {
+		app.get(key,require(index.get[key]));
+	}
+}
+if("post" in index) {
+	for(var key in index.get) {
+		app.post(key,require(index.get[key]));
+	}
+}
 
 app.listen(80,function() {
 	console.log("app is running at port 80.");
